4f38295fa8396ece142667f869c5f09085e5e8bc
Darren Ou <dushu.ou@gmail.com>
2022-02-10 14:46:43 +0800

    Add files

--- 148.排序链表.cpp
+++ 148.排序链表.cpp
@@ -25,40 +25,80 @@ private:
         }
         ListNode *slow = head;
         ListNode *fast = head;
-        while (fast != tail) {
+        while (fast != tail && fast->next != tail) {
+            fast = fast->next->next;
             slow = slow->next;
-            fast = fast->next;
-            if (fast != tail) {
-                fast = fast->next;
-            }
         }
         return merge(mySortList(head, slow), mySortList(slow, tail));
     }
 
-    ListNode *merge(ListNode *head1, ListNode *head2) {
-        ListNode *node1 = head1;
-        ListNode *node2 = head2;
-        ListNode *tempNode = new ListNode();
-        ListNode *cur = tempNode;
-        while (node1 && node2) {
-            if (node1->val <= node2->val) {
-                cur->next = node1;
-                node1 = node1->next;
+    ListNode *merge(ListNode *l1, ListNode *l2) {
+        ListNode *node = new ListNode();
+        ListNode *cur = node;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
             } else {
-                cur->next = node2;
-                node2 = node2->next;
+                cur->next = l2;
+                l2 = l2->next;
             }
             cur = cur->next;
         }
-        if (node1) cur->next = node1;
-        if (node2) cur->next = node2;
-        return tempNode->next;
+        if (l1) cur->next = l1;
+        if (l2) cur->next = l2;
+        return node->next;
     }
-
 public:
     ListNode* sortList(ListNode* head) {
         return mySortList(head, nullptr);
     }
 };
+
+// class Solution {
+// private:
+//     ListNode *mySortList(ListNode *head, ListNode *tail) {
+//         if (!head) return head;
+//         if (head->next == tail) {
+//             head->next = nullptr;
+//             return head;
+//         }
+//         ListNode *slow = head;
+//         ListNode *fast = head;
+//         while (fast != tail) {
+//             slow = slow->next;
+//             fast = fast->next;
+//             if (fast != tail) {
+//                 fast = fast->next;
+//             }
+//         }
+//         return merge(mySortList(head, slow), mySortList(slow, tail));
+//     }
+
+//     ListNode *merge(ListNode *head1, ListNode *head2) {
+//         ListNode *node1 = head1;
+//         ListNode *node2 = head2;
+//         ListNode *tempNode = new ListNode();
+//         ListNode *cur = tempNode;
+//         while (node1 && node2) {
+//             if (node1->val <= node2->val) {
+//                 cur->next = node1;
+//                 node1 = node1->next;
+//             } else {
+//                 cur->next = node2;
+//                 node2 = node2->next;
+//             }
+//             cur = cur->next;
+//         }
+//         if (node1) cur->next = node1;
+//         if (node2) cur->next = node2;
+//         return tempNode->next;
+//     }
+
+// public:
+//     ListNode* sortList(ListNode* head) {
+//         return mySortList(head, nullptr);
+//     }
+// };
 // @lc code=end
 
